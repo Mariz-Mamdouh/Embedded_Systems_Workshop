200c4afe612a560630db2a06da16c2c15f2bd116
Mariz Mamdouh <143612992+Mariz-Mamdouh@users.noreply.github.com>
2023-09-05 21:30:43 +0300

    Add files via upload

---
+++ COTS/STM32F103C8/MCAL/RCC/RCC_interface.h
@@ -0,0 +1,11 @@
+/****************************************************/
+/*********   Author    : Mariz Mamdouh   ************/
+/*********   Date      : 28 Aug 2023     ************/
+/*********   Version   : 0.1             ************/
+/*********   File name : RCC_interface.h ************/
+/****************************************************/
+#ifndef RCC_INTERFACE_H
+#define RCC_INTERFACE_H
+
+
+#endif /*RCC_INTERFACE_H*/
